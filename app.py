490cad941482168b0390bbd538314fbe8c85a77b
nishimura-calendar <fumihiro.nishimura@gmail.com>
2026-03-24 12:24:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import io
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 from google.oauth2 import service_account
-from practice_1 import pdf_reader, extract_year_month, time_schedule_from_drive, data_integration
+from practice_0 import pdf_reader, extract_year_month, time_schedule_from_drive, data_integration
 
 # 設定値
 TIME_TABLE_ID = "1p7EBN1zTTt09etuQkZTIXBlNutUZqQkG"
