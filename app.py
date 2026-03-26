a11ca215fb59f36c977fcd23d25ba303e78a8db7
nishimura-calendar <fumihiro.nishimura@gmail.com>
2026-03-26 16:59:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import unicodedata
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from web_practice_0 import (
+from practice_0 import (
     pdf_reader, extract_year_month, time_schedule_from_drive, 
     data_integration, parse_special_shift
 )
