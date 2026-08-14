5c2da8a48fa19bafc8ed366e7e0b93a5662e0342
nishimura-calendar <fumihiro.nishimura@gmail.com>
2026-08-14 23:24:06 +0900

    Update subject assignment in final row processing
    Refactor final row subject assignment logic.

--- app.py
+++ app.py
@@ -341,11 +341,11 @@ if uploaded_pdf:
                                 # 以降に業務がない場合は「退勤」扱い
                                 end = "(退勤)"
                             end_time=time_shift.iloc[0, t_col]
-    
-                            # 最終行の不定項目をセット
-                            subject=final_rows[0][0] 
-                            final_rows[0][0]=subject+break_change+end
                             final_rows[0][4] = end_time                            
+    
+                        # 最終行の不定項目をセット
+                        subject=subject+break_change+end 
+                        final_rows[0][0]=subject+break_change+end
                         
                     prev_val = current_val
                     
